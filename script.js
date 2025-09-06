20e8381c561c691cce53d8963d527da4c230bcb7
wellcomemayhem-spec <hirajfaryar@hotmail.com>
2025-09-07 02:58:37 +0800

    Fix potential issue with writing to the iframe when codeArea is empty.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ previewFrame.addEventListener('load', () => {
     const doc = previewFrame.contentDocument || previewFrame.contentWindow.document;
     codeArea.addEventListener('input', () => {
         const code = codeArea.value;
-        if (doc) {
+        if (doc && code.trim() !== '') {
             doc.open();
             doc.write(code);
             doc.close();
