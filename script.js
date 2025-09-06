c77a8982e48254cda44e0ae7e62bb627bdaefb90
wellcomemayhem-spec <hirajfaryar@hotmail.com>
2025-09-07 02:57:31 +0800

    Fix: Added a check for iframe to prevent errors if not loaded yet.

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const previewFrame = document.getElementById('preview');
 codeArea.addEventListener('input', () => {
     const code = codeArea.value;
     const doc = previewFrame.contentDocument || previewFrame.contentWindow.document;
-    doc.open();
-    doc.write(code);
-    doc.close();
+    if (doc) {
+        doc.open();
+        doc.write(code);
+        doc.close();
+    }
 });
